dd376587109c75b1868314f6de16650e8023b381
thiago-igor <thiago.maia0606@gmail.com>
2024-01-06 11:42:04 -0300

    Novo projeto (Formularios em react)

---
+++ 5_FORM_EM_REACT/form/src/components/MyForm.jsx
@@ -0,0 +1,19 @@
+import './MyForm.css'
+
+const MyForm = () => {
+  return (
+    <div>
+      {/* 1 - Criação de form */}
+
+      <form>
+        <div>
+            <label htmlFor="name">Nome: </label>
+            <input type="text" name='name' placeholder='Digite seu nome'/>
+        </div>
+        <input type="submit" value="Enviar" />
+      </form>
+    </div>
+  )
+}
+
+export default MyForm
