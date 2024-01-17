95b3829ac0e4c14d9179399ece6f02e4808dddf6
thiago-igor <thiago.maia0606@gmail.com>
2024-01-17 10:23:55 -0300

    Atualização
    Trazendo dados da API

--- 7_TEQ_HTTP/httpreact/src/App.jsx
+++ 7_TEQ_HTTP/httpreact/src/App.jsx
@@ -3,10 +3,30 @@ import './App.css'
 import { useState, useEffect } from 'react';
 
 function App() {
+  const[products, setProducts] = useState([]) // array de produtos
+  const url = "http://localhost:3000/products" // url dos produtos na API
 
+  // 1 - Resgatando dados:
+  useEffect(() => {
+
+    async function fetchData(){
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data)
+    }
+    fetchData();
+  }, []);
   return (
     <div className="app">
       <h1>Lista de produtos</h1>
+
+      <ul>
+        {products.map((product) => ( // "for" de todos os produtos 
+          <li key={product.id}> {product.name} - R$: {product.price} </li>
+        ))}
+      </ul>
     </div>
   )
 }
